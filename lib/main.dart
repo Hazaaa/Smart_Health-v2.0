66bb2836088476aa68b6a07bf2e8bac10c831e55
Hazaaa <stefanacimovic1117@gmail.com>
2021-08-14 21:50:10 +0200

    Added required packages and config files for Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_health_v2/presentation/screens/login_screen.dart';
 import 'package:smart_health_v2/constants/size_confige.dart';
 import 'package:smart_health_v2/routes/router.dart' as router;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
